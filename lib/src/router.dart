8278c5972fb408672461e1ba87b0fd18ad2f9eb8
米珐 <stephenwzl.wzl@antfin.com>
2018-04-25 20:12:32 +0800

    [update] complete controller router method map

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -1,5 +1,5 @@
 import 'dart:mirrors';
-
+import 'dart:async';
 /// route annotation
 class Route {
   final String path;
@@ -11,26 +11,49 @@ class Route {
   const Route.DELETE(String path) : this(path, 'DELETE');
   const Route.OPTION(String path) : this(path, 'OPTION');
   const Route.PUT(String path) : this(path, 'PUT');
+
+  String toString() => '$method:$path';
 }
-// abstract router
+
 
 class Router {
-  Map<String, Map> _router = {};
+  static final Router _instance = new Router._internal();
 
+  factory Router() {
+    return _instance;
+  }
+
+  /// private constructor
+  Router._internal();
+
+  Map<String, Map<String, Symbol>> router = {};
+  Map<String, Object> _controllers = {};
+
+  /// use this regex to judge if a function is async function
+  RegExp _asyncFuncRegex = new RegExp('async\ *\n*{');
+
+  /// register a controller into router map
   void registerController(Object controllerIns, String path) {
+    // push a controller into memory
+    _controllers[path] = controllerIns;
+
     var klass = reflectClass(controllerIns.runtimeType);
+    Map<String, Symbol> controllerMethodMap = {};
     klass.declarations.forEach((Symbol symbol, DeclarationMirror declare) {
       if (declare is MethodMirror) {
         MethodMirror method = declare;
         if (method.isRegularMethod &&
             !method.isAbstract &&
-            !method.isOperator) {
-          var routeAnnotation = _getAnnotation(method, Route);
+            !method.isOperator &&
+            method.source.contains(_asyncFuncRegex)) {
+          Route routeAnnotation = _getAnnotation(method, Route);
           if (routeAnnotation != null) {
+            controllerMethodMap[routeAnnotation.toString()] = method.simpleName;
           }
         }
       }
     });
+    router[path] = controllerMethodMap;
   }
 
   /// get annotation for controller route method
